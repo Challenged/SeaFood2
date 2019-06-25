0c4d33bd9a61e42803c595a6ef1d54892087d3d1
Rustam <d.rustam.o@gmail.com>
2019-06-25 15:42:15 +0300

    Initial Commit

---
+++ SeaFood2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeaFood2
+//
+//  Created by Rustam on 6/25/19.
+//  Copyright © 2019 Rustam Duspayev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
