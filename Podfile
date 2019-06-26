8b212d723e211c454b0c7b17794516b392657016
Rustam <d.rustam.o@gmail.com>
2019-06-26 15:57:45 +0300

    Initiate model, implement recognition function, use colors with ChameleonFramework

---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '9.0'
+
+target 'SeaFood2' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for SeaFood2
+
+pod 'ChameleonFramework/Swift', :git => 'https://github.com/ViccAlexander/Chameleon.git'
+
+end
--- SeaFood2/ViewController.swift
+++ SeaFood2/ViewController.swift
@@ -7,14 +7,87 @@
 //
 
 import UIKit
+import CoreML
+import Vision
+import ChameleonFramework
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    let imagePicker = UIImagePickerController()
+
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+
+    @IBOutlet weak var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = false
+
     }
 
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            imageView.image = userPickedImage
+            guard let ciImage = CIImage(image: userPickedImage) else {fatalError("Couldn't convert UIImage to CIImage")}
+
+            detect(image: ciImage)
+        }
+
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+
+    func detect(image: CIImage) {
+
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {fatalError("Loading coreML model failed")}
+
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+
+            guard let resuts = request.results as? [VNClassificationObservation] else {fatalError("Image processing failed")}
+
+//            print(resuts)
+
+            if resuts.first!.identifier.contains("hotdog") {
+                self.updateNavBar(withColor: FlatMint(), text: "Hotdog!")
+            } else {
+                self.updateNavBar(withColor: FlatPinkDark(), text: "Not Hotdog!")
+            }
+
+        }
+
+        let handler = VNImageRequestHandler(ciImage: image)
+
+        do {
+            try handler.perform([request])
+        } catch {
+            print("Error processing the request by model \(error)")
+        }
+        
+    }
+
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+
+        present(imagePicker, animated: true, completion: nil)
+
+    }
+
+    func updateNavBar(withColor color: UIColor, text: String) {
+
+        guard let navBar = navigationController?.navigationBar else {fatalError("No navigation bar found")}
+
+        navigationItem.title = text
+
+        navBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: ContrastColorOf(color, returnFlat: true)]
+
+        navBar.barTintColor = color
+
+        navBar.tintColor = ContrastColorOf(color, returnFlat: true)
+
+    }
 
 }
 
